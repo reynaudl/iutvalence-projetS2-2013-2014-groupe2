093bd2939fc7aeae9faa284fe48aa486d36e8a35
dupuis jeremy <jeremy.dupuis@iut-valence.fr>
2014-04-17 13:53:38 +0200

    Creation des principales classe du projet. Pour l'instant vide
    Jérémy/Adem/Loic/Thai binh

---
+++ TheLostScrollOfIUT/src/iutvalence_projetS2_2013_2014_groupe2_ScrollIUT/Item.java
@@ -0,0 +1,5 @@
+package iutvalence_projetS2_2013_2014_groupe2_ScrollIUT;
+
+public class Item {
+
+}
---
+++ TheLostScrollOfIUT/src/iutvalence_projetS2_2013_2014_groupe2_ScrollIUT/ScrollIUT.java
@@ -0,0 +1,10 @@
+package iutvalence_projetS2_2013_2014_groupe2_ScrollIUT;
+
+public class ScrollIUT {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
