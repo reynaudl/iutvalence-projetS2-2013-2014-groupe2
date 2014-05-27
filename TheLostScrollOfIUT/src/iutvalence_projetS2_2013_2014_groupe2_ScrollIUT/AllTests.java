ef55608ee589e488c95e96ade4e0a683c484454f
Kadtalyc <Kadtalyc@BigBrother-PC>
2014-05-27 06:58:02 +0200

    Ajout de test, problème sur l'appelle des constructeurs et des méthodes des classes à tester.

---
+++ TheLostScrollOfIUT/src/iutvalence_projetS2_2013_2014_groupe2_ScrollIUT/AllTests.java
@@ -0,0 +1,17 @@
+package iutvalence_projetS2_2013_2014_groupe2_ScrollIUT;
+
+import junit.framework.TestSuite;
+
+public class AllTests {
+	public static TestSuite suite() {
+		TestSuite suite = new TestSuite();
+		//suite.addTest(CompteurTest.suite());
+		
+		return suite;
+	}
+
+//	public static void main(String[] args) {
+//		junit.textui.TestRunner.run(suite());
+//	}
+
+}
